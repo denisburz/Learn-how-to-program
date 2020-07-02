25eee4a2c773c75af07a30817f985c63e52f3376
Catalin Denis Burz <catalin.burz@outlook.com>
2020-07-02 16:26:34 +0300

    Add a new project named CifreRomane

--- HelloWorld/HelloWord/Program.cs
+++ HelloWorld/HelloWord/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Introduce-ti primul numar:");
+            Console.WriteLine("Introduce-ti al doinea numar:");
+
+            var first = Console.ReadLine();
+            var second = Console.ReadLine();
+
+            Console.WriteLine(second);
         }
     }
 }
